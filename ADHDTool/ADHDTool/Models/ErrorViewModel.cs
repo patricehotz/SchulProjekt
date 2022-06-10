8b0fd7d685f491a6d910afe32b34dabe95f5af79
Patrice Hotz <patrice.hotz@leuchterag.ch>
2022-06-10 09:44:43 +0200

    Basic Layout

---
+++ ADHDTool/ADHDTool/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ADHDTool.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
